0ca641388154b0ab52b44f644da5885fd5fb7244
JasonZheng <zhengzhentao27@126.com>
2023-12-15 15:50:13 +0800

    update

--- assets1/js/parallelCoordinates.js
+++ assets1/js/parallelCoordinates.js
@@ -8,69 +8,74 @@ const axis = {
     zIndex: 1,
     titlePosition: 'right',
     line: true,
-    labelStroke: '#fff',
-    labelStrokeWidth: 5,
-    labelFontSize: 10,
-    labelStrokeLineJoin: 'round',
-    titleStroke: '#fff',
-    titleFontSize: 10,
-    titleStrokeWidth: 5,
-    titleStrokeLineJoin: 'round',
+    labelStroke: '#ffffff',
+    labelFill:'#ffffff',
+    labelStrokeWidth: 2,
+    labelFontSize: 25,
+    labelStrokeLineJoin: 'bevel',
+    titleStroke: '#ffffff',
+    titleFill:'#ffffff',
+    titleFontSize: 20,
+    titleStrokeWidth: 2,
+    titleStrokeLineJoin: 'bevel',
     titleTransform: 'translate(-50%, 0) rotate(-90)',
-    lineStroke: 'black',
-    tickStroke: 'black',
-    lineStrokeWidth: 1,
+    lineStroke: 'White',
+    tickStroke: 'White',
+    lineStrokeWidth: 2,
   };
   
-  const chart = new G2.Chart({
-    container: 'parallel-container',
-    autoFit: true,
-    height:1050
-  });
-  
-  chart.coordinate({ type: 'parallel' });
-  
-  chart
-    .line()
-    .data({
-      type: 'fetch',
-      value: 'https://assets.antv.antgroup.com/g2/cars3.json',
-    })
-    .encode('position', [
-      'economy (mpg)',
-      'cylinders',
-      'displacement (cc)',
-      'power (hp)',
-      'weight (lb)',
-      '0-60 mph (s)',
-      'year',
-    ])
-    .encode('color', 'weight (lb)')
-    .style('strokeWidth', 1.5)
-    .style('strokeOpacity', 0.4)
-    .scale('color', {
-      palette: 'inferno',
-      offset: (t) => 1 - t,
-    })
-    .legend({
-      color: { length: 400, layout: { justifyContent: 'center' } },
-    })
-    .axis('position', axis)
-    .axis('position1', axis)
-    .axis('position2', axis)
-    .axis('position3', axis)
-    .axis('position4', axis)
-    .axis('position5', axis)
-    .axis('position6', axis)
-    .axis('position7', axis);
-  
-  // // 放大图例字体
-  // chart.legend('weight (lb)', {
-  //   style: {
-  //     fontSize: 2000,  // 根据需要设置字体大小
-  //   },
-  // });  
-  
-  chart.interaction('tooltip', { series: false });
-  
-  chart.render();
+const chart = new G2.Chart({
+  container: 'parallel-container',
+  autoFit: true,
+  height:1020,
+  width:1750,
+});
+
+chart.coordinate({ type: 'parallel' });
+
+chart
+  .line()
+  .data({
+    type: 'fetch',
+    value: 'assets1/data/paralleltemp.json',
+  })
+  // 轴标签
+  .encode('position', [  
+    'economy (mpg)',
+    'cylinders',
+    'displacement (cc)',
+    'power (hp)',
+    'weight (lb)',
+    '0-60 mph (s)',
+    'year',
+  ])
+  // 颜色映射到weight(lb)
+  .encode('color', 'weight (lb)')
+  // 线的宽度和不透明度
+  .style('strokeWidth', 2)
+  .style('strokeOpacity', 0.5)
+  // 设置颜色比例尺和调色板
+  .scale('color', {
+    palette: 'inferno',
+    offset: (t) => 1 - t,
+  })
+  .legend({
+    color: { length: 400, layout: { justifyContent: 'center' } },
+    handle:true,
+    slidable:true,
+    labelFontSize:20,
+    labelStroke:'#ffffff'
+  })
+  .axis('position', axis)
+  .axis('position1', axis)
+  .axis('position2', axis)
+  .axis('position3', axis)
+  .axis('position4', axis)
+  .axis('position5', axis)
+  .axis('position6', axis)
+  .axis('position7', axis);
+
+
+chart.interaction('tooltip', { series: false });
+
+chart.render();
