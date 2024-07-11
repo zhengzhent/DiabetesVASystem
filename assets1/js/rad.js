15e3a7fbe3cb8188bfbdbafdbcba73f4b4a9f627
JasonZheng <zhengzhentao27@126.com>
2024-07-11 14:32:31 +0800

    update

--- assets1/js/rad.js
+++ assets1/js/rad.js
@@ -25,8 +25,8 @@ const normalizedData = data.map(item => ({
 }));
     data = normalizedData
 
-    const width = 590; // 调整宽度
-    const height = 705; // 调整高度
+    const width = 820; // 调整宽度
+    const height = 500; // 调整高度
     const margin = { top: 50, right: 50, bottom: 50, left: 50 };
     const radius = Math.min(width, height) / 2 - Math.max(margin.top, margin.right, margin.bottom, margin.left);
 
@@ -141,8 +141,8 @@ function drawChart2(){
         { axis: "SmokingHistory", value: 0.037274160026708955 }
     ];
 
-    const width = 500; // 调整宽度
-    const height = 805; // 调整高度
+    const width = 820; // 调整宽度
+    const height = 500; // 调整高度
     const margin = { top: 50, right: 50, bottom: 50, left: 50 };
     const radius = Math.min(width, height) / 2 - Math.max(margin.top, margin.right, margin.bottom, margin.left);
     const values = data.map(item => item.value);
@@ -286,8 +286,8 @@ const normalizedData = data.map(item => ({
 }));
     data = normalizedData
 
-    const width = 500; // 调整宽度
-    const height = 805; // 调整高度
+    const width = 820; // 调整宽度
+    const height = 500; // 调整高度
     const margin = { top: 50, right: 50, bottom: 50, left: 50 };
     const radius = Math.min(width, height) / 2 - Math.max(margin.top, margin.right, margin.bottom, margin.left);
 
